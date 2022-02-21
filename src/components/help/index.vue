cbface16ee70e6ead9ccc35b3a40608efe55f898
lin <franklinmagoba4@gmail.comgit config --global user.email franklinmagoba4@gmail.com>
2022-02-21 14:03:05 +0300

    added layout

---
+++ src/components/help/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="help">
+    <img :src="image" class="help-img" />
+    <span class="tooltiptext">{{ titleText }}</span>
+  </div>
+</template>
+<script>
+import image from "../../assets/icons/clarity_help-line.svg";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Help",
+  props: {
+    titleText: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  data() {
+    return {
+      image,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "./index.css";
+</style>
