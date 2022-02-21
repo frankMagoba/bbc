eb64d4af98b41564c73c34acc3e0b080aa5fa30c
lin <franklinmagoba4@gmail.comgit config --global user.email franklinmagoba4@gmail.com>
2022-02-21 14:27:05 +0300

    CITY tests

---
+++ src/components/city/tests/unit/index.spec.js
@@ -0,0 +1,25 @@
+import CityDetails from "../../index.vue";
+import { shallowMount } from "@vue/test-utils";
+
+describe("TEST CITY DETAILS COMPONENT", () => {
+  const details = [{ name: "deli", aqi: "22.5", cigg: "2" }];
+  const wrapper = shallowMount(CityDetails, {
+    propsData: {
+      cityDetails: details,
+    },
+  });
+
+  test("CHECKING IF THE EXPECT TEXTS ARE RENDERED FROM PROPS", () => {
+    const cityName = wrapper.find(".titlename");
+    const cityAqi = wrapper.find(".titleaqi");
+    const cityCigg = wrapper.find(".titlecigg");
+    expect(cityName.text()).toEqual(details[0].name);
+    expect(cityAqi.text()).toEqual(details[0].aqi);
+    expect(cityCigg.text()).toEqual(details[0].cigg);
+  });
+  test("CHECKING IF CLICK TRIGGERS AN EVENT", async () => {
+    const closeItem = wrapper.find(".closeItem");
+    await closeItem.trigger("click");
+    expect(wrapper.emitted().closeAll).toBeTruthy();
+  });
+});
